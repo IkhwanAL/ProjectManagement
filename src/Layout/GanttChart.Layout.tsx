e4078c9e1b1d25d4960622d6046a76af82dc5dfd
ikhwan A Latif <ikhwanal235@gmail.com>
2022-05-23 09:31:48 +0700

    adding Google Charts

--- src/Layout/GanttChart.Layout.tsx
+++ src/Layout/GanttChart.Layout.tsx
@@ -1,1 +1,74 @@
-export {};
+import React from "react";
+import { Chart } from "react-google-charts";
+
+export default function GanttChart() {
+	function daysToMilliseconds(days: number) {
+		return days * 24 * 60 * 60 * 1000;
+	}
+
+	const columns: Array<{ type: string; label: string }> = [
+		{ type: "string", label: "Task ID" },
+		{ type: "string", label: "Task Name" },
+		{ type: "date", label: "Start Date" },
+		{ type: "date", label: "End Date" },
+		{ type: "number", label: "Duration" },
+		{ type: "number", label: "Percent Complete" },
+		{ type: "string", label: "Dependencies" },
+	];
+
+	const rows = [
+		[
+			"Research",
+			"Find sources",
+			new Date(2015, 0, 1),
+			new Date(2015, 0, 5),
+			null,
+			100,
+			null,
+		],
+		[
+			"Write",
+			"Write paper",
+			null,
+			new Date(2015, 0, 9),
+			daysToMilliseconds(3),
+			25,
+			"Research,Outline",
+		],
+		[
+			"Cite",
+			"Create bibliography",
+			null,
+			new Date(2015, 0, 7),
+			daysToMilliseconds(1),
+			20,
+			"Research",
+		],
+		[
+			"Complete",
+			"Hand in paper",
+			null,
+			new Date(2015, 0, 10),
+			daysToMilliseconds(1),
+			0,
+			"Cite,Write",
+		],
+		[
+			"Outline",
+			"Outline paper",
+			null,
+			new Date(2015, 0, 6),
+			daysToMilliseconds(1),
+			100,
+			"Research",
+		],
+	];
+
+	const data = [columns, ...rows];
+
+	return (
+		<>
+			<Chart chartType="Gantt" width="100%" data={data} />
+		</>
+	);
+}
